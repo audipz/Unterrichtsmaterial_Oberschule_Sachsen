238fc54c52f0b6dc5c4fb86bcc3dd2988172cba3
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 12:25:30 +0200

    refactor: ungenutzte Spring-Data-Repositories entfernen

--- Lernplattform/backend/persistence/src/main/java/de/schule/informatik/lernplattform/persistence/repository/SchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package de.schule.informatik.lernplattform.persistence.repository;
-
-import de.schule.informatik.lernplattform.persistence.entity.SchoolEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SchoolRepository extends JpaRepository<SchoolEntity, UUID> {
-}
