d20ec776d72763a1ec4c9fda17d0ceb5e66809a6
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 13:58:28 +0200

    Expose public school registration functional endpoint

---
+++ Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/app/web/functional/PublicRoutes.java
@@ -0,0 +1,25 @@
+package de.schule.informatik.lernplattform.app.web.functional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.accept;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@Configuration(proxyBeanMethods = false)
+public class PublicRoutes {
+
+    @Bean
+    RouterFunction<ServerResponse> publicRegistrationRoutes(PublicRegistrationHandler handler) {
+        return route()
+                .POST(
+                        "/api/v1/public/school-registrations",
+                        accept(MediaType.APPLICATION_JSON).and(contentType(MediaType.APPLICATION_JSON)),
+                        handler::submitSchoolRegistration)
+                .build();
+    }
+}
