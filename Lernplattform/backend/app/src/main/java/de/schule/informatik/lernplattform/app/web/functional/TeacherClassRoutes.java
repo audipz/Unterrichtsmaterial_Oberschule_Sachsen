ca45b1e6aa989aa81b6cd531d4bf53dde0b18ee7
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 18:05:28 +0200

    Expose teacher student overview

--- Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/app/web/functional/TeacherClassRoutes.java
+++ Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/app/web/functional/TeacherClassRoutes.java
@@ -14,10 +14,12 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 public class TeacherClassRoutes {
 
     @Bean
-    RouterFunction<ServerResponse> teacherClassRoutes(TeacherClassHandler handler) {
+    RouterFunction<ServerResponse> teacherClassRoutes(TeacherClassHandler handler,
+                                                       TeacherStudentHandler studentHandler) {
         var json = accept(MediaType.APPLICATION_JSON).and(contentType(MediaType.APPLICATION_JSON));
         return route()
                 .GET("/api/v1/schools/{school}/classes", handler::listClasses)
+                .GET("/api/v1/schools/{school}/students", studentHandler::listStudents)
                 .GET("/api/v1/schools/{school}/classes/{classId}/students", handler::listStudents)
                 .GET("/api/v1/schools/{school}/classes/{classId}/teachers", handler::listTeachers)
                 .GET("/api/v1/schools/{school}/teachers", handler::listAvailableTeachers)
