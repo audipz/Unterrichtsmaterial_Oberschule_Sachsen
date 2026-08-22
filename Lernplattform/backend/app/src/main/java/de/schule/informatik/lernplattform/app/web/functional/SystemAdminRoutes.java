fb5ee14ee091423c9d520ff48b6db39b09ac13d1
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 14:32:17 +0200

    Route system admin school registration review

---
+++ Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/app/web/functional/SystemAdminRoutes.java
@@ -0,0 +1,25 @@
+package de.schule.informatik.lernplattform.app.web.functional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.accept;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@Configuration(proxyBeanMethods = false)
+public class SystemAdminRoutes {
+
+    @Bean
+    RouterFunction<ServerResponse> systemAdminRegistrationRoutes(SystemAdminRegistrationHandler handler) {
+        var json = accept(MediaType.APPLICATION_JSON).and(contentType(MediaType.APPLICATION_JSON));
+        return route()
+                .GET("/api/v1/system-admin/school-registrations", handler::listPending)
+                .POST("/api/v1/system-admin/school-registrations/{requestId}/approve", handler::approve)
+                .POST("/api/v1/system-admin/school-registrations/{requestId}/reject", json, handler::reject)
+                .build();
+    }
+}
