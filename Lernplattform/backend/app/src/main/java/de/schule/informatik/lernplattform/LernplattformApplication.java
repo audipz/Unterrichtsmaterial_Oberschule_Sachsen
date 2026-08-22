ae28c824c5051ed5e34be932320e47b24ccc0165
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 14:13:42 +0200

    Enable scheduled application tasks

--- Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/LernplattformApplication.java
+++ Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/LernplattformApplication.java
@@ -3,8 +3,10 @@ package de.schule.informatik.lernplattform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LernplattformApplication {
 
     public static void main(String[] args) {
