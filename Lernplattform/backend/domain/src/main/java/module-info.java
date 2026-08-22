1a3dba078ccdb944177623d39376b425bddf6602
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 11:29:10 +0200

    refactor: Domain als Java-25-Modul definieren

---
+++ Lernplattform/backend/domain/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module de.schule.informatik.lernplattform.domain {
+    exports de.schule.informatik.lernplattform.domain.auth;
+    exports de.schule.informatik.lernplattform.domain.displayname;
+    exports de.schule.informatik.lernplattform.domain.school;
+    exports de.schule.informatik.lernplattform.domain.schoolclass;
+    exports de.schule.informatik.lernplattform.domain.user;
+}
