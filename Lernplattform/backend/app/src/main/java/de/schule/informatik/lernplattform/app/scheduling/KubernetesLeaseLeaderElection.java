2c58a64e216ad1311d58303af83074fdc3f94919
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 14:40:36 +0200

    Migrate lease JSON handling to Jackson 3

--- Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/app/scheduling/KubernetesLeaseLeaderElection.java
+++ Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/app/scheduling/KubernetesLeaseLeaderElection.java
@@ -1,7 +1,7 @@
 package de.schule.informatik.lernplattform.app.scheduling;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
