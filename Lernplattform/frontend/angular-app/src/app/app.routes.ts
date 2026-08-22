e172a57ad980c44db2b1dd50b45dcaa4d2a7c37c
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 18:35:00 +0200

    Route school scoped student login before generic shell

--- Lernplattform/frontend/angular-app/src/app/app.routes.ts
+++ Lernplattform/frontend/angular-app/src/app/app.routes.ts
@@ -19,6 +19,10 @@ export const routes: Routes = [
     loadComponent: () => import('./system-admin/system-admin.component').then((m) => m.SystemAdminComponent)
   },
   {
+    path: ':school/login',
+    loadComponent: () => import('./student-login/student-login.component').then((m) => m.StudentLoginComponent)
+  },
+  {
     path: ':school/:view',
     loadComponent: () => import('./shell/app-shell.component').then((m) => m.AppShellComponent)
   },
