6842a233a7d3596f418ebea22bb075a523767793
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 18:31:30 +0200

    Add student authentication routes

---
+++ Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/app/web/functional/StudentAuthRoutes.java
@@ -0,0 +1,24 @@
+package de.schule.informatik.lernplattform.app.web.functional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RequestPredicates.accept;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@Configuration(proxyBeanMethods = false)
+public class StudentAuthRoutes {
+    @Bean
+    RouterFunction<ServerResponse> studentAuthRoutes(StudentAuthHandler handler) {
+        var json = accept(MediaType.APPLICATION_JSON).and(contentType(MediaType.APPLICATION_JSON));
+        return route()
+                .POST("/api/v1/schools/{school}/student-auth/login", json, handler::login)
+                .POST("/api/v1/student-auth/change-password", json, handler::changePassword)
+                .POST("/api/v1/student-auth/logout", handler::logout)
+                .build();
+    }
+}
