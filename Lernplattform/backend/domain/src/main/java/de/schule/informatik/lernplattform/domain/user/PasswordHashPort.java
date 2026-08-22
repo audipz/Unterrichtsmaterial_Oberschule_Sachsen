dbf10bbad1ccd336dc482951318beb7d6b2dd502
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 14:46:23 +0200

    Support password verification

--- Lernplattform/backend/domain/src/main/java/de/schule/informatik/lernplattform/domain/user/PasswordHashPort.java
+++ Lernplattform/backend/domain/src/main/java/de/schule/informatik/lernplattform/domain/user/PasswordHashPort.java
@@ -3,4 +3,6 @@ package de.schule.informatik.lernplattform.domain.user;
 public interface PasswordHashPort {
 
     String hash(String rawPassword);
+
+    boolean matches(String rawPassword, String encodedPassword);
 }
