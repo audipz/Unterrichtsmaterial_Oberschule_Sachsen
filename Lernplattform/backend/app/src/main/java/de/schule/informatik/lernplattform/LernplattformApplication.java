687bf3496c436d07897e739033e8b443f7caa561
Holger Graube <15184610+audipz@users.noreply.github.com>
2026-08-22 13:26:26 +0200

    Add migration-only startup mode

--- Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/LernplattformApplication.java
+++ Lernplattform/backend/app/src/main/java/de/schule/informatik/lernplattform/LernplattformApplication.java
@@ -1,12 +1,24 @@
 package de.schule.informatik.lernplattform;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LernplattformApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LernplattformApplication.class, args);
+        boolean migrationOnly = Boolean.parseBoolean(
+                System.getenv().getOrDefault("MIGRATION_ONLY", "false"));
+
+        SpringApplication application = new SpringApplication(LernplattformApplication.class);
+        if (migrationOnly) {
+            application.setWebApplicationType(WebApplicationType.NONE);
+        }
+
+        var context = application.run(args);
+        if (migrationOnly) {
+            context.close();
+        }
     }
 }
